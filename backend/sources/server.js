45c9c5b8e1b120f71b99efd7480fa578e22e734e
João Pedro <TocooXD@gmail.com>
2026-09-19 18:28:35 -0300

    chore: configura aplicação para deploy no Render

--- backend/sources/server.js
+++ backend/sources/server.js
@@ -1,3 +1,5 @@
 import app from "./app"; // Importa o server que foi exportado pelo app.js.
 
-app.listen(3333); // Inicia o servidor na porta 3333.
+const port = process.env.PORT || 3333;
+
+app.listen(port); // Inicia o servidor na porta 3333.
